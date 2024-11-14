c62dbf4b0d3dde16f376c2d08ab3e2601a70854a
𝗧𝗛𝗔𝗥𝗨𝗦𝗛𝗔 𝗦𝗔𝗡𝗗𝗜𝗣𝗔 <166241288+mageekabn@users.noreply.github.com>
2024-11-14 20:12:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "Put Session Id",
+  SESSION_ID: process.env.SESSION_ID || "FKNk1ZJD#_cJ0u_2Bd-M9MgdDYjk_9DQVeo3Q22Rmv5FfQeeEK2Y",
   // ADD Your Session Id 
   MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
     // ADD Your MongoDB Database URL
